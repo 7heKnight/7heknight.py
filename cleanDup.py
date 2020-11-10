8536bceffc3e3960abbc78c69a0049660f8b39f0
7heKnight <60308445+7heKnight@users.noreply.github.com>
2020-11-10 13:51:10 +0700

    Update cleanDup.py

--- cleanDup.py
+++ cleanDup.py
@@ -1,4 +1,11 @@
+#The Code will hash the data of files and add it into an array, and them compare with others file, if exist, it will remove that file. It will make a file(.log) in the
+# same folder
+#Not recommended using it in root directory
+#Maked and tested on Windows 10 pro 20H2 | Python Version: 3.9.0
+#How To Use: python sys.argv[0] <Directory>. E.g: python cleanDup.py C:\Users\User_name\Desktop
+
 import hashlib
+import time
 import sys
 import os
 
@@ -24,12 +31,12 @@ def openAndCheckDir(dir):
                         listHash.append(h)
                     else:
                         count += 1
-                        log.write('\n'+str(count)+'. ' + root + name)
-                        print('[-] Removed ' + root + name)
+                        log.write('\n'+str(count)+'. ' + root + '\\' + name)
+                        print('[-] Removed ' + root + '\\' + name)
                         os.remove(f)
             except:
                 countScan += 1
-                print('[-] Could not scan the file: ' + root+name)
+                print('[-] Could not scan the file: ' + root + '\\' + name)
     log.write('\n')
     log.close()
     return count, countScan
@@ -39,19 +46,24 @@ def hashMake(Files):
     return string
 
 if __name__=='__main__':
+    first = time.clock()
     dir = sys.argv[1]
     if os.path.isdir(dir):
         count, cS = openAndCheckDir(dir)
         print('\n[+] Scanned ' + str(cS) + ' files')
         if count == 0:
             print('\n[*] No files duplicated')
-            exit('\n[-] Teminate')
+            print('\n[-] Teminate')
+            exit('The program processed in ' + str(time.clock() - first) + ' sec')
         else:
             print('\n[-] Removed ' + str(count) + ' duplicated files')
-            exit('[-] Teminate')
+            print('[-] Teminate')
+            exit('The program processed in ' + str(time.clock() - first) + ' sec')
     elif os.path.isfile(dir):
         print('\nUsage: ' + sys.argv[0] + ' <Directory>')
-        exit('\n[-] Teminated. Application can not scan the path')
+        print('\n[-] Teminated. Application can not scan the path')
+        exit('The program processed in ' + str(time.clock() - first) + ' sec')
     else:
         print('\nUsage: ' + sys.argv[0] + ' <Directory>')
-        exit('\n[-] Directory not found. Terminated')
+        print('\n[-] Directory not found. Terminated')
+        exit('The program processed in ' + str(time.clock() - first) + ' sec')
