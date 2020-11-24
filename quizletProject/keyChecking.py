8b19ae4222e14d21d69e384fdf89db2027007412
7heKnight <60308445+7heKnight@users.noreply.github.com>
2020-11-24 23:59:37 +0700

    Update keyChecking.py

--- quizletProject/keyChecking.py
+++ quizletProject/keyChecking.py
@@ -1,42 +1,43 @@
+#Usage: 1. python File.py <FileDirection>
+#       2. Python File.py
 import pathlib
 import sys
 import re
 import os
 
 def parser(fileDir):
-    answer = []
+    listQA = []
     path = pathlib.Path(fileDir)
     pathName = str(pathlib.Path(fileDir)).strip(str(pathlib.Path(fileDir).name))
-    f = open(fileDir, 'r', encoding="utf-8")
+    f = open(fileDir, 'r', encoding='UTF-8')
     for i in f:
-        result = re.sub(r'\w{1,5}[=]{1,3}\d{0,10}[ ]{0,10}', '', i.split('\n')[0])
-        afterResult = re.sub(r'[a-eA-E]{1}[./]{1,99}[ ]{0,99}', '', result)
-        answer.append(afterResult)
+        result = re.sub(r'\w{1,5}[=]{1,3}\d{0,10}[ \t]{1,10}', '', i.split('\n')[0])
+        afterResult = re.sub(r'[a-eA-E0-9]{1,3}[./]{1,99}[ \t]{1,99}', '', result)
+        listQA.append(str(afterResult))
     f.close()
-    return answer, pathName, str(path.name)
+    return listQA, str(pathName), str(path.name)
 
 if __name__=='__main__':
-    a = []
-    q = []
+    listQA = []
     fileDir = None
     path = 0
     name = 0
     if len(sys.argv) == 2:
         fileDir = sys.argv[1]
         if os.path.isfile(fileDir):
-            a, path, name = parser(fileDir)
+            listQA, path, name = parser(fileDir)
         else:
             exit('[-] You need input the Path + File_Name')
     elif len(sys.argv) == 1:
         fileDir = input('[*] Enter File Direction: ')
         if os.path.isfile(fileDir):
-            a, path, name = parser(fileDir)
+            listQA, path, name = parser(fileDir)
         else:
             exit('[-] You need input the Path + File_Name')
     else:
         exit('[-] Terminated')
-    f = open(path + 'Copied_' + name, 'w')
-    for i in a:
+    f = open(path + 'Copied_' + name, 'w', encoding='UTF-8')
+    for i in listQA:
         try:
             f.write(i+'\n')
         except:
