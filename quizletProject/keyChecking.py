dc15016baeea935fa96c6e7aee64303e21c15eba
7heKnight <60308445+7heKnight@users.noreply.github.com>
2020-11-25 17:19:13 +0700

    Update keyChecking.py

--- quizletProject/keyChecking.py
+++ quizletProject/keyChecking.py
@@ -14,7 +14,11 @@ def parser(fileDir):
     for i in f:
         result = re.sub(r'\w{1,5}[=]{1,3}\d{0,10}[ \t]{1,10}', '', i.split('\n')[0])
         afterResult = re.sub(r'[a-eA-E0-9]{1,3}[./]{1,99}[ \t]{1,99}', '', result)
-        listQA.append(str(afterResult))
+        afterAfterResult = re.sub(r'[\t]{1,9}', '', afterResult)
+        afterAfterResult1 = re.sub(r'[0-9\-\[\]]{2,99}[ ][a-zA-Z0-9]{1,5}]', '', afterAfterResult)
+        afterAfterResult2 = re.sub(r'[0-9\-\[\]]{2,99}[ ]', '', afterAfterResult1)
+        afterAfterResult3 = re.sub(r'[ ]{2,99}', '', afterAfterResult2)
+        listQA.append(str(afterAfterResult3))
     f.close()
     return listQA, str(pathName), str(path.name)
 
