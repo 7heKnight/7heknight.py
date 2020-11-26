9e275ecab28497af325a70b66778aa93b751e5ea
7heKnight <60308445+7heKnight@users.noreply.github.com>
2020-11-26 13:19:09 +0700

    Update keyChecking.py

--- quizletProject/keyChecking.py
+++ quizletProject/keyChecking.py
@@ -12,13 +12,18 @@ def parser(fileDir):
     pathName = str(pathlib.Path(fileDir)).strip(str(pathlib.Path(fileDir).name))
     f = open(fileDir, 'r', encoding='UTF-8')
     for i in f:
-        result = re.sub(r'\w{1,5}[=]{1,3}\d{0,10}[ \t]{1,10}', '', i.split('\n')[0])
-        afterResult = re.sub(r'[a-eA-E0-9]{1,3}[./]{1,99}[ \t]{1,99}', '', result)
-        afterAfterResult = re.sub(r'[\t]{1,9}', '', afterResult)
-        afterAfterResult1 = re.sub(r'[0-9\-\[\]]{2,99}[ ][a-zA-Z0-9]{1,5}]', '', afterAfterResult)
-        afterAfterResult2 = re.sub(r'[0-9\-\[\]]{2,99}[ ]', '', afterAfterResult1)
-        afterAfterResult3 = re.sub(r'[ ]{2,99}', '', afterAfterResult2)
-        listQA.append(str(afterAfterResult3))
+        r = i.split('\n')[0]
+        if '|' not in r: # if there is no '|' char in array it would be printed and does not add to list
+            print('[-] Removed ' + r + '_$') # $ mean end of string
+        else:
+            rr = re.sub(r'^[0-9\-) ]{2,5}', '', r) # 1-Question
+            result = re.sub(r'\w{1,5}[=]{1,3}\d{0,10}[ \t]{1,10}[0-9()]{0,9}[ \t]{0,3}', '', rr) # Q1=123[space or tab](123-456)[space or tab]
+            afterResult = re.sub(r'[a-eA-E0-9]{1,3}[./]{1,99}[ \t]{1,99}', '', result) # abc[. or /][space or tab]
+            afterAfterResult = re.sub(r'[\t]{1,9}', '', afterResult) # if equal or more 1 tab, it will remove
+            afterAfterResult1 = re.sub(r'[0-9\-\[\]]{2,99}[ ][a-zA-Z0-9]{1,5}]', '', afterAfterResult) # [100] T
+            afterAfterResult2 = re.sub(r'[0-9\-\[\]]{2,99}[ ]', '', afterAfterResult1) # [100]
+            afterAfterResult3 = re.sub(r'[ ]{2,99}', ' ', afterAfterResult2) # more than 2 space will place to 1 space
+            listQA.append(str(afterAfterResult3))
     f.close()
     return listQA, str(pathName), str(path.name)
 
@@ -42,6 +47,7 @@ if __name__=='__main__':
     else:
         exit('[-] Terminated')
     f = open(path + 'Copied_' + name, 'w', encoding='UTF-8')
+    print('\n[+] Coping to created file: ' + path + 'Copied_' + name)
     for i in listQA:
         try:
             f.write(i+'\n')
