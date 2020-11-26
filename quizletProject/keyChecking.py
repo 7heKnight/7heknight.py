55341a7eb0e89a39c115fc7b4c28284ece041e5f
7heKnight <60308445+7heKnight@users.noreply.github.com>
2020-11-26 17:06:34 +0700

    Update keyChecking.py

--- quizletProject/keyChecking.py
+++ quizletProject/keyChecking.py
@@ -1,6 +1,7 @@
 # This tool created to remove some annoying key like: a. b. c. , a/ b/ c/,....
 # Usage: 1. python File.py <FileDirection>
 #        2. Python File.py
+
 import pathlib
 import time
 import sys
@@ -11,10 +12,7 @@ def parser(fileDir):
     listQA = []
     path = pathlib.Path(fileDir)
     pathName = str(pathlib.Path(fileDir)).strip(str(pathlib.Path(fileDir).name))
-    try:
-        f = open(fileDir, 'r', encoding='UTF-8')
-    except:
-        exit("[-] You should save the file with Encode:'UTF-8'")
+    f = open(fileDir, 'r', encoding='UTF-8')
     for i in f:
         r = i.split('\n')[0]
         keyParser = i.replace('\n', '')
@@ -22,20 +20,21 @@ def parser(fileDir):
         keyParser = keyParser.split('|')
         try:
             if '|' not in r:
-                print('[-] Removed the wrong format line: ' + r + '_$') # $ mean end of string
+                print('[-] Removed the wrong format line: ' + r + '_$')
             elif not keyParser[0] == r'' and not keyParser[1] == r'':
-                rr = re.sub(r'^[0-9\-#./) ]{2,5}', '', r)  # 1-Question
-                result = re.sub(r'\w{1,5}[=]{1,3}\d{0,10}[ \t]{1,10}[0-9()]{0,9}[ \t]{0,3}', '', rr)  # Q1=123[space or tab](123-456)[space or tab]
-                afterResult = re.sub(r'[a-eA-E0-9]{1,3}[./]{1,99}[ \t]{1,99}', '', result)  # abc[. or /][space or tab]
-                afterAfterResult = re.sub(r'[\t]{1,9}', '', afterResult)  # if equal or more 1 tab, it will remove
-                afterAfterResult1 = re.sub(r'[0-9\-\[\]]{2,99}[ ][a-zA-Z0-9]{1,5}]', '', afterAfterResult)  # [100] T
-                afterAfterResult2 = re.sub(r'[0-9\-\[\]]{2,99}[ ]', '', afterAfterResult1)  # [100]
-                afterAfterResult3 = re.sub(r'[ ]{2,99}', ' ', afterAfterResult2)  # more than 2 space will place to 1 space
-                listQA.append(str(afterAfterResult3))
+                result = re.sub(r'^[0-9\-#./: ) ]{1,5}', '', r)
+                result = re.sub(r'\|[ ]{0,2}[(0-9) ]{0,5}', '| ', result)
+                result = re.sub(r'\w{1,5}[=]{1,3}\d{0,10}[ \t]{1,10}[0-9()]{0,9}[ \t]{0,3}', '', result) 
+                result = re.sub(r'[a-eA-E0-9]{1,3}[./]{1,99}[ \t]{1,99}', '', result)
+                result = re.sub(r'[\t]{1,9}', '', result)
+                result = re.sub(r'[0-9\-\[\]]{2,99}[ ][a-zA-Z0-9]{1,5}]', '', result)
+                result = re.sub(r'[0-9\-\[\]]{2,99}[ ]', '', result)
+                result = re.sub(r'[ ]{2,99}', ' ', result)
+                listQA.append(str(result))
             else:
                 print('[-] Removed the wrong format line: ' + r + '_$')
         except:
-            print('[...........]')
+            pass
     f.close()
     return listQA, str(pathName), str(path.name)
 
