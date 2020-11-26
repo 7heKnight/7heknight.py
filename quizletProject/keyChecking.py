cbd41aca8e1928b2be6d21f04d37cf680f76ca6a
7heKnight <60308445+7heKnight@users.noreply.github.com>
2020-11-26 13:31:08 +0700

    Update keyChecking.py

--- quizletProject/keyChecking.py
+++ quizletProject/keyChecking.py
@@ -16,13 +16,14 @@ def parser(fileDir):
         if '|' not in r: # if there is no '|' char in array it would be printed and does not add to list
             print('[-] Removed ' + r + '_$') # $ mean end of string
         else:
-            rr = re.sub(r'^[0-9\-) ]{2,5}', '', r) # 1-Question
+            rr = re.sub(r'^[0-9\-#./) ]{2,5}', '', r) # 1-Question
             result = re.sub(r'\w{1,5}[=]{1,3}\d{0,10}[ \t]{1,10}[0-9()]{0,9}[ \t]{0,3}', '', rr) # Q1=123[space or tab](123-456)[space or tab]
             afterResult = re.sub(r'[a-eA-E0-9]{1,3}[./]{1,99}[ \t]{1,99}', '', result) # abc[. or /][space or tab]
             afterAfterResult = re.sub(r'[\t]{1,9}', '', afterResult) # if equal or more 1 tab, it will remove
             afterAfterResult1 = re.sub(r'[0-9\-\[\]]{2,99}[ ][a-zA-Z0-9]{1,5}]', '', afterAfterResult) # [100] T
             afterAfterResult2 = re.sub(r'[0-9\-\[\]]{2,99}[ ]', '', afterAfterResult1) # [100]
             afterAfterResult3 = re.sub(r'[ ]{2,99}', ' ', afterAfterResult2) # more than 2 space will place to 1 space
+            print('[-] Removed ' + afterAfterResult3 + '_$')
             listQA.append(str(afterAfterResult3))
     f.close()
     return listQA, str(pathName), str(path.name)
