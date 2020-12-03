5a12911a3c71ead6f5c348d748cad4d9e26bf746
7heKnight <60308445+7heKnight@users.noreply.github.com>
2020-12-04 03:28:56 +0700

    Update convertQ2K.py

--- quizletProject/convertQ2K.py
+++ quizletProject/convertQ2K.py
@@ -3,6 +3,7 @@ import sys
 import re
 
 def readFile(dir):
+    key = None
     rawList = []
     try:
         file = open('key.txt', 'r', encoding='UTF-8')
@@ -10,7 +11,6 @@ def readFile(dir):
         file.close()
     except:
         pass
-
     try:
         f = open(dir, 'r', encoding='UTF-8')
         rawList.append(str(f.read()).split('**'))
@@ -29,17 +29,10 @@ def type2FirstParse(rawList):
     for i in range(len(rawList)):
         q = rawList[i].split('~~')[0].replace('\n\n', '\n').replace('  ', ' ') # Changed here
         a = rawList[i].replace('\n', '').replace(' ', '').split('~~')[1].lower()
-        forRegex = re.split(r'[\n ]{1}[a-gA-G]{1}[,.)]{1}[ ]{0,1}', q, flags=re.IGNORECASE) #here
+        forRegex = re.split(r'[\n\[ ]{1}[a-gA-G]{1}[,.)\]]{1}[ ]{0,1}', q, flags=re.IGNORECASE) #here
         questions.append(forRegex[0].replace('\n', ' '))
         questionAnswer.append(forRegex)
         answers.append(a.lower())
-
-        # if error sth, print here
-        # print(answers)
-        # print(questions)
-        # print(questionAnswer)
-        # print(finalList)
-
     return finalList, questions, questionAnswer, answers
 
 def parseText_Type2(dir):
@@ -65,10 +58,16 @@ def parseText_Type2(dir):
             answerAfterParse.append(boolCheck.get(answers[i]))
 
     for i in range(len(questions)):
-        sub = re.sub('^[ ]{0,1}[0-9a-zA-Z]{1,2}[.)]{1}[ ]{1}', '', questions[i].replace('  ', ' '))
-        sub = re.sub('^[ ]{1,2}', '', sub)
+        sub = re.sub('^[ \[]{0,1}[0-9a-zA-Z]{1,2}[.)\]]{1}[ ]{1}', '', questions[i].replace('  ', ' '))
+        sub = re.sub('^[ ~]{1,2}', '', sub)
+        sub = re.sub('^[0-9]{1,3}[)]{1}[ ]{1}', '', sub)
+        sub = re.sub('^[0-9]{1,4}[/. ]{1,3}', '', sub)
+        sub = re.sub('^[A-Z]{1,3}[=]{1,2}[0-9]{1,4}[ ]{0,1}', '', sub)
+        sub = re.sub('^[(]{1}[0-9]{1,9}[)]{1}[ ]{1}', '', sub)
         try:
-            if not (str(sub) == '' and answerAfterParse[i] == ''):
+            stringQ = str(sub)
+            stringA = answerAfterParse[i]
+            if not stringQ.replace(' ', '') == '' and not stringA.replace(' ', '') == '':
                 keyParsed = str(sub) + '|' + answerAfterParse[i]
                 if keyParsed in key:
                     pass
@@ -85,11 +84,6 @@ def parseText_Type2(dir):
                     finalList.append(keyParsed + '\n')
             else:
                 errorList.append(str(sub)+'|')
-    # if error sth, print here
-    # print(answers)
-    # print(questions)
-    # print(questionAnswer)
-    # print(finalList)
     return finalList, errorList
 
 def type1FirstParse(key, rawList):
@@ -115,6 +109,60 @@ def parseText_Type1(dir):
     finalList = type1FirstParse(key, rawList)
     return finalList
 
+def type3(dir):
+    file = open(dir, 'r', encoding='UTF-8')
+    arguments = file.read()
+    arguments = re.sub('[*]{3,99}', '**', arguments)
+    arguments = re.sub('[~]{3,99}', '**', arguments)
+    file.close()
+    arguments = arguments.split('**')
+    arguments.pop()
+    count = 0
+    try:
+        file = open(dir, 'w', encoding='UTF-8')
+        for i in arguments:
+            file.write(i.split('~~')[1] + '~~' + i.split('~~')[0] + '**')
+            count += 1
+        file.close()
+    except:
+        exit('[-] Error unknown.')
+    print('\n[+] Overrided position of ' + str(count) + ' lines into File: ' + dir)
+
+def type4(dir):
+    count = 0
+    dup = 0
+    formE = 0
+    keyList = []
+
+    file = open(dir, 'r', encoding='UTF-8')
+    arguments = file.read()
+    file.close()
+    arguments = arguments.split('\n')
+    arguments.pop()
+
+    for i in arguments:
+        try:
+            if not i.split('|')[0] == '' and not i.split('|')[1] == '':
+                if i.replace(' ', '') in keyList:
+                    dup += 1
+                else:
+                    keyList.append(i + '\n')
+            else:
+                print('[-] Wrong format: '+i)
+                formE += 1
+        except:
+            print('[-] Wrong format1: ' + i)
+            formE += 1
+    try:
+        file = open(dir+'.log', 'w', encoding='UTF-8')
+        for i in keyList:
+            file.write(i)
+            count += 1
+        file.close()
+    except:
+        exit('[-] Error unknown.')
+    print('\n[+] Overrided ' + str(count) + ' key and ' + str(dup) + ' duplicated key and ' + str(formE) + ' key in wrong form.')
+
 if __name__=='__main__':
     start = time.time()
     file = open('key.txt', 'a', encoding='UTF-8')
@@ -138,6 +186,12 @@ if __name__=='__main__':
                 file.write(i)
                 countKey += 1
             print('\n[+] Keys total wrote to file: ' + str(countKey))
+        elif sys.argv[1] == r'3':
+            type3(sys.argv[2])
+        elif sys.argv[1] == r'4':
+            type4(sys.argv[2])
+        else:
+            exit('[-] Option not found!')
     else:
         exit('[-] Wrong format. Usage: python ' + str(sys.argv[0]) + ' (type of raw question) <rawList.txt>')
     print('-------------------------------------------------------------')
