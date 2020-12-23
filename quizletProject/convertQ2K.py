eb0b54d743589dcd848fcddc32a5cf2f63bb3568
7heKnight <60308445+7heKnight@users.noreply.github.com>
2020-12-23 19:37:06 +0700

    Update convertQ2K.py

--- quizletProject/convertQ2K.py
+++ quizletProject/convertQ2K.py
@@ -1,21 +1,30 @@
 import time
 import sys
 import re
+HELP = fr'''
+[*] Usage: python {sys.argv[0]} <Option number> <Question and answer in raw>
+    - Option 1: Parsing the question and answer which is have the format like flashcard. It will pass the key if duplicated.
+    - Option 2: Parsing the question have the multichoice and answer is only character. It will pass the key if duplicated.
+    - Option 3: Swaping the position of question and answer.
+    - Option 4: Checking and removing key if each key in wrong form.
 
+** Notes: Option have no duplicated checking so if you want to check, just use tool of Notepad++ (Edit >> Line Operation >> Remove duplicated lines.)\
+*** Export note: Custom on the left must be "~~" and the right custom is "**".
+'''
 def readFile(dir):
     key = None
     rawList = []
     try:
         file = open('key.txt', 'r', encoding='UTF-8')
         key = file.read()
-        key = re.sub('[*]{3,99}', '**', key)
-        key = re.sub('[~]{3,99}', '~~', key)
+        key = re.sub('[*]{3,99}', '', key)
+        key = re.sub('[~]{3,99}', '', key)
         file.close()
     except:
         pass
     try:
         f = open(dir, 'r', encoding='UTF-8')
-        rawList.append(str(f.read()).split('**'))
+        rawList.append(str(f.read()).split('"**"'))
         rawList[0].pop()
         rawList = rawList[0]
         f.close()
@@ -23,14 +32,75 @@ def readFile(dir):
         print('[-] Error while opening files.')
     return key, rawList
 
+def parseQuestion_Type1(key, rawList):
+    finalList = []
+    for i in range(len(rawList)):
+        question = rawList[i].replace('\n', ' ').replace('  ', ' ').split('"~~"')[0]
+        question = re.sub(r'^[# ]{1,2}', '', question)
+        question = re.sub(r'^[tT]/[fF][ ]{0,2}', '', question)
+        question = re.sub(r'^[_]{1,99}[ ]{0,1}', '', question)
+        question = re.sub(r'^[0-9]{1,3}[.) ]{1,3}', '', question)
+        question = re.sub(r'^[a-z]{1,2}[=]{1}[0-9]{1,3}[ ]{1,2}', '', question, flags=re.IGNORECASE)
+        question = re.sub(r'[|]{1,99}', '', question)
+        question = re.sub(r'[ :.,]{1,99}$', '', question)
+        answer = rawList[i].replace('\n', ' ').replace('  ', ' ').split('"~~"')[1]
+        answer = re.sub(r'[|]{1,99}', '', answer)
+        question = re.sub(r'[_]{1,99}[ .]{0,1}$', '', question)
+        answer = re.sub(r'[ :.,]{1,99}$', '', answer)
+        keyParsed = str(question)+ '|' + str(answer)
+        if not (question == '' and answer == ''):
+            if keyParsed in key:
+                pass
+            else:
+                finalList.append(keyParsed + '\n')
+        else:
+            pass
+    return finalList
+
+def parseText_Type1(dir):
+    key, rawList = readFile(dir)
+    finalList = parseQuestion_Type1(key, rawList)
+    return finalList
+
+def parseQuestion_Type2(questions, key):
+    sub = re.sub(r'^[ \[]{0,1}[0-9a-zA-Z]{1,2}[.)\]]{1}[ ]{1}', '', questions.replace('  ', ' ').replace('|', ''))
+    sub = re.sub(r'^[tT]/[fF][ ]{0,2}', '', sub)
+    sub = re.sub(r'[,. ]{0,2}[sS][eE][lL][eE][cC][tT][ ]{0,1}[oO][nN][eE][: ]{0,3}', '', sub)
+    sub = re.sub(r'^[_]{1,99}[ ]{0,1}', '', sub)
+    sub = re.sub(r'^Question[:]{0,1}[ ]{1,2}[0-9]{1,4}|question[:]{0,1}[ ]{1,2}[0-9]{1,4}|QUESTION[:]{0,1}[ ]{1,2}[0-9]{1,4}', '', sub, re.IGNORECASE)
+    sub = re.sub(r'^[ ~:]{1,99}', '', sub)
+    sub = re.sub(r'^[0-9]{1,3}[)]{1}[ ]{1}', '', sub)
+    sub = re.sub(r'^[0-9]{1,4}[/. ]{1,3}', '', sub)
+    sub = re.sub(r'^[A-Z]{1,3}[=]{1,2}[0-9]{1,4}[ ]{0,1}', '', sub)
+    sub = re.sub(r'^[(]{1}[0-9]{1,9}[)]{1}[ ]{1}', '', sub)
+    sub = re.sub(r'[*: ]{1,2}$', '', sub).replace('|', '')
+    sub = re.sub(r'[*:]{1,2}[ ]{0,3}$', '', sub).replace('|', '')
+    key = re.sub(r'[01]{1}/1', '', key).replace('  ', ' ')
+    return sub, key
+
+def parseQuestion_Type4(i):
+    i = re.sub(r'^[TF/ ]{4}', '', i)
+    i = re.sub(r'^[_]{1,99}[ ]{0,1}', '', i)
+    i = re.sub(r'^[a-zA-Z]{2,3}[=]{1,2}[0-9 ]{1,5}', '', i)
+    i = re.sub(r'^[qQ][uU][eE][sS][tT][iI][oO][nN][sS]{0,1}[:]{0,1}[ ]{1,2}[0-9]{1,4}', '', i)
+    i = re.sub(r'^[(]{1}[0-9]{1,9}[) ]{1,3}', '', i)
+    i = re.sub(r'^[#~: ]{1,4}', '', i)
+    i = re.sub(r'^[0-9]{1,4}[/). ]{1,2}', '', i)
+    i = re.sub(r'^[ ]{1,99}', '', i)
+    i = re.sub(r'[*,: ]{1,2}[|]{1}', '|', i)
+    i = re.sub(r'[|]{1}[ ]{1,9}', '|', i)
+    i = re.sub(r'[ ]{2,99}', ' ', i)
+    i = re.sub(r'[ (]{1,2}[*]{1}[) ]{1,2}[|]{1}', '|', i)
+    return i
+
 def type2FirstParse(rawList):
     answers = []
     questions = []
     finalList = []
     questionAnswer = []
     for i in range(len(rawList)):
-        q = rawList[i].split('~~')[0].replace('\n\n', '\n').replace('  ', ' ') # Changed here
-        a = rawList[i].replace('\n', '').replace(' ', '').split('~~')[1].lower()
+        q = rawList[i].split('"~~"')[0].replace('\n\n', '\n').replace('  ', ' ') # Changed here
+        a = rawList[i].replace('\n', '').replace(' ', '').split('"~~"')[1].lower()
         forRegex = re.split(r'[\n\[ ]{1}[a-gA-G]{1}[,.)\]]{1}[ ]{0,1}', q, flags=re.IGNORECASE) #here
         questions.append(forRegex[0].replace('\n', ' '))
         questionAnswer.append(forRegex)
@@ -60,15 +130,10 @@ def parseText_Type2(dir):
             answerAfterParse.append(boolCheck.get(answers[i]))
 
     for i in range(len(questions)):
-        sub = re.sub('^[ \[]{0,1}[0-9a-zA-Z]{1,2}[.)\]]{1}[ ]{1}', '', questions[i].replace('  ', ' '))
-        sub = re.sub('^[ ~]{1,2}', '', sub)
-        sub = re.sub('^[0-9]{1,3}[)]{1}[ ]{1}', '', sub)
-        sub = re.sub('^[0-9]{1,4}[/. ]{1,3}', '', sub)
-        sub = re.sub('^[A-Z]{1,3}[=]{1,2}[0-9]{1,4}[ ]{0,1}', '', sub)
-        sub = re.sub('^[(]{1}[0-9]{1,9}[)]{1}[ ]{1}', '', sub)
+        sub ,key = parseQuestion_Type2(questions[i], key)
         try:
             stringQ = str(sub)
-            stringA = answerAfterParse[i]
+            stringA = re.sub(r'[ ]{1,99}$', '', answerAfterParse[i])
             if not stringQ.replace(' ', '') == '' and not stringA.replace(' ', '') == '':
                 keyParsed = str(sub) + '|' + answerAfterParse[i]
                 if keyParsed in key:
@@ -88,42 +153,19 @@ def parseText_Type2(dir):
                 errorList.append(str(sub)+'|')
     return finalList, errorList
 
-def type1FirstParse(key, rawList):
-    finalList = []
-    for i in range(len(rawList)):
-        question = rawList[i].replace('\n', ' ').replace('  ', ' ').split('~~')[0]
-        question = re.sub('^[# ]{1,2}', '', question)
-        question = re.sub('^[0-9]{1,3}[.) ]{1,3}', '', question)
-        question = re.sub('^[a-z]{1,2}[=]{1}[0-9]{1,3}[ ]{1,2}', '', question, flags=re.IGNORECASE)
-        answer = rawList[i].replace('\n', ' ').replace('  ', ' ').split('~~')[1]
-        keyParsed = str(question) + '|' + str(answer)
-        if not (question == '' and answer == ''):
-            if keyParsed in key:
-                pass
-            else:
-                finalList.append(keyParsed + '\n')
-        else:
-            pass
-    return finalList
-
-def parseText_Type1(dir):
-    key, rawList = readFile(dir)
-    finalList = type1FirstParse(key, rawList)
-    return finalList
-
 def type3(dir):
     file = open(dir, 'r', encoding='UTF-8')
     arguments = file.read()
-    arguments = re.sub('[*]{3,99}', '**', arguments)
-    arguments = re.sub('[~]{3,99}', '~~', arguments)
+    arguments = re.sub('[*]{3,99}', '', arguments)
+    arguments = re.sub('[~]{3,99}', '', arguments)
     file.close()
-    arguments = arguments.split('**')
+    arguments = arguments.split('"**"')
     arguments.pop()
     count = 0
     try:
         file = open(dir, 'w', encoding='UTF-8')
         for i in arguments:
-            file.write(i.split('~~')[1] + '~~' + i.split('~~')[0] + '**')
+            file.write(i.split('"~~"')[1] + '"~~"' + i.split('"~~"')[0] + '"**"')
             count += 1
         file.close()
     except:
@@ -132,12 +174,12 @@ def type3(dir):
 
 def type4(dir):
     count = 0
-    dup = 0
     formE = 0
     keyList = []
-    
+
     file = open(dir, 'r', encoding='UTF-8')
     arguments = file.read()
+    arguments = re.sub(r'[ ]{1,99}[\n]{1}', '\n', arguments)
     file.close()
     arguments = arguments.split('\n')
     arguments.pop()
@@ -145,25 +187,25 @@ def type4(dir):
     for i in arguments:
         try:
             if not i.split('|')[0] == '' and not i.split('|')[1] == '':
-                if i.replace(' ', '') in keyList:
-                    dup += 1
-                else:
-                    keyList.append(i + '\n')
+                keyList.append(i + '\n')
             else:
-                print('[-] Wrong format: '+i)
+                # print('[-] Wrong format: '+i)
+                print(i)
                 formE += 1
         except:
-            print('[-] Wrong format1: ' + i)
+            # print('[-] Wrong format1: ' + i)
+            print(i)
             formE += 1
     try:
         file = open(dir+'.log', 'w', encoding='UTF-8')
         for i in keyList:
+            i = parseQuestion_Type4(i.replace('  ', ' '))
             file.write(i)
             count += 1
         file.close()
     except:
         exit('[-] Error unknown.')
-    print('\n[+] Overrided ' + str(count) + ' key and ' + str(dup) + ' duplicated key and ' + str(formE) + ' key in wrong form.')
+    print('\n[+] Overrided ' + str(count) + ' keys and ' + str(formE) + ' key in wrong form.')
 
 if __name__=='__main__':
     start = time.time()
@@ -193,12 +235,12 @@ if __name__=='__main__':
         elif sys.argv[1] == r'4':
             type4(sys.argv[2])
         else:
-            exit('[-] Option not found!')
+            exit('\n[-] Option not found!\n' + HELP)
     else:
-        exit('[-] Wrong format. Usage: python ' + str(sys.argv[0]) + ' (type of raw question) <rawList.txt>')
+        exit('\n[-] Wrong format.\n'+HELP)
     print('-------------------------------------------------------------')
     end = time.time() - start
     time.sleep(0.0000000001)
     exit('Program executed in ' + str(end) + ' seconds')
 
-# Notes: This is Code just applied with the parser for the answer is: ~~ and next question is: **
+# Notes: This is Code just applied with the parser for the answer is: "~~" and next question is: "**"
