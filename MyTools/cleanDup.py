20c14b6cb768c4fe9c864a126dc94a40148f7202
7heKnight <60308445+7heKnight@users.noreply.github.com>
2021-05-05 11:44:31 +0700

    Update cleanDup.py

--- MyTools/cleanDup.py
+++ MyTools/cleanDup.py
@@ -1,8 +1,8 @@
-#The Code will hash the data of file and put it into an array, and them compare with others file, if exist, it will remove that file. It will make a file(.log) in the
+# The Code will hash the data of file and put it into an array, and them compare with others file, if exist, it will remove that file. It will make a file(.log) in the
 # same folder
-#Not recommended using it in root directory
-#Maked and tested on Windows 10 pro 20H2 | Python Version: 3.9.0
-#How To Use: python sys.argv[0] <Directory>. E.g: python cleanDup.py C:\Users\User_name\Desktop
+# Not recommended using it in root directory
+# Maked and tested on Windows 10 pro 20H2 | Python Version: 3.9.0
+# How To Use: python sys.argv[0] <Directory>. E.g: python cleanDup.py C:\Users\User_name\Desktop
 
 import hashlib
 import time
@@ -13,14 +13,14 @@ def openAndCheckDir(dir):
     listHash = []
     count = 0
     countScan = 0
-    log = open(sys.argv[0] + '.log', 'a+')
+    log = open(sys.argv[0] + '.log', 'a+', encoding='UTF-8')
     for root, dirs, files in os.walk(dir, topdown=True):
         isScanning = False
         for name in files:
             try:
                 if isScanning == False:
                     isScanning = True
-                    print('\n[+] Scanning the directory: '+ root+'\n')
+                    print('\n[+] Scanning the directory: ' + root +'\n')
                 f = os.path.join(root, name)
                 ff = open(f, 'rb')
                 h = hashMake(ff.read())
@@ -34,19 +34,18 @@ def openAndCheckDir(dir):
                         log.write('\n'+str(count)+'. ' + root + '\\' + name)
                         print('[-] Removed ' + root + '\\' + name)
                         os.remove(f)
-            except:
+            except KeyboardInterrupt:
                 countScan += 1
-                print('[-] Could not scan the file: ' + root + '\\' + name)
+                print('[!] Could not scan the file: ' + root + '\\' + name)
     log.write('\n')
     log.close()
     return count, countScan
 
 def hashMake(Files):
-    string = hashlib.sha1(Files).hexdigest()
-    return string
+    return hashlib.sha1(Files).hexdigest()
 
 if __name__=='__main__':
-    first = time.clock()
+    first = time.time()
     dir = sys.argv[1]
     if os.path.isdir(dir):
         count, cS = openAndCheckDir(dir)
@@ -54,16 +53,16 @@ if __name__=='__main__':
         if count == 0:
             print('\n[*] No files duplicated')
             print('\n[-] Teminate')
-            exit('The program processed in ' + str(time.clock() - first) + ' sec')
+            exit('The program processed in ' + str(time.time() - first) + ' sec')
         else:
             print('\n[-] Removed ' + str(count) + ' duplicated files')
             print('[-] Teminate')
-            exit('The program processed in ' + str(time.clock() - first) + ' sec')
+            exit('The program processed in ' + str(time.time() - first) + ' sec')
     elif os.path.isfile(dir):
         print('\nUsage: ' + sys.argv[0] + ' <Directory>')
         print('\n[-] Teminated. Application can not scan the path')
-        exit('The program processed in ' + str(time.clock() - first) + ' sec')
+        exit('The program processed in ' + str(time.time() - first) + ' sec')
     else:
         print('\nUsage: ' + sys.argv[0] + ' <Directory>')
         print('\n[-] Directory not found. Terminated')
-        exit('The program processed in ' + str(time.clock() - first) + ' sec')
+        exit('The program processed in ' + str(time.time() - first) + ' sec')
