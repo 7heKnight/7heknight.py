fce06a95cdb38e35e4f1807c05e51540f583cfb7
7heKnight <60308445+7heKnight@users.noreply.github.com>
2021-06-08 06:22:22 +0000

    Update password_generator.py

--- MyTools/password_generator.py
+++ MyTools/password_generator.py
@@ -6,11 +6,14 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         sys.exit(f'[-] Missing Length for password. E.g: {sys.argv[0]} 10')
-    characters = string.digits + string.ascii_letters
     try:
+        characters_raw = string.digits + string.ascii_letters
+        characters = ''
         if int(sys.argv[1]) > len(characters):
-            spawn = int(int(sys.argv[1])/len(characters))
-            characters = ''.join(characters+characters for i in range(0, int(spawn)))
+            spawn = int(int(sys.argv[1])/len(characters_raw))
+            if len(characters_raw) * spawn - int(sys.argv[1]) < 0:
+                spawn += 1
+            characters = ''.join(characters+characters_raw for i in range(0,int(spawn)))
         password_length = int(sys.argv[1])
         password = "".join(random.sample(characters, password_length))
         print(f"Gernerated password: {password}")
