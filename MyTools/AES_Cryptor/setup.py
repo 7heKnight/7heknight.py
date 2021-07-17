468155248dae0c1be83f8fe50f51646d7a8458c7
7heKnight <60308445+7heKnight@users.noreply.github.com>
2021-07-17 14:15:33 +0700

    Update setup.py

--- MyTools/AES_Cryptor/setup.py
+++ MyTools/AES_Cryptor/setup.py
@@ -4,3 +4,6 @@ lib = ['PyCryptodome']
 for i in lib:
     print(f'\n[*] Installing {i}...')
     os.system(f'pip install {i}')
+os.system(f'mkdir AES_Cryptor')
+os.system(f'curl https://raw.githubusercontent.com/7heKnight/7heknight.py/main/MyTools/AES_Cryptor/AES_Encrypt.py > AES_Cryptor/AES_Encrypt.py')
+os.system(f'curl https://raw.githubusercontent.com/7heKnight/7heknight.py/main/MyTools/AES_Cryptor/AES_Decrypt.py > AES_Cryptor/AES_Decrypt.py')
