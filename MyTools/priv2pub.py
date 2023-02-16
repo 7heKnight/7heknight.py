a24fb0c5fa73c0569aafe14a56478ae6ce0ec660
Ly Tuan Kiet <60308445+7heKnight@users.noreply.github.com>
2023-02-16 15:45:22 +0700

    Add files via upload

---
+++ MyTools/priv2pub.py
@@ -0,0 +1,18 @@
+from os.path import isfile
+from os import remove
+from sys import argv, exit
+import subprocess
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print("Public Key Required!")
+        exit(-1)
+    try:
+        open('tmp_data', 'w').write(f'-----BEGIN RSA PRIVATE KEY-----\n{argv[1]}\n-----END RSA PRIVATE KEY-----\n')
+        output = subprocess.getoutput('openssl rsa -in tmp_data -pubout')
+        print(output.replace('writing RSA key', ''))
+    except:
+        pass
+
+    if isfile('tmp_data'):
+        remove('tmp_data')
